040a428724188d4a8c3ded5d733a763fadd41257
Alberto Ruiz <aruiz@um.es>
2006-03-21 12:30:03 +0000

    added examples/garbage.hs

---
+++ examples/garbage.hs
@@ -0,0 +1,21 @@
+-- checking automatic garbage collection
+
+import GSL
+import GSL.Util
+
+m k = constant k (1000000::Int)
+
+-- this is an infinite list of one million-component vectors
+x :: [V]
+x = map m [0..]
+
+-- this is an infinite list with the corresponding euclidean norms
+y :: [Double]
+y = map norm x
+
+-- the same list, the intermediate vectors are not kept
+z :: [Double]
+z = map (norm . m) [0..]
+
+main = do
+    print z  -- No problem, but please DO NOT print y
