bcd4372a428bf720fa40b7d29170f8dfee900ecd
Alberto Ruiz <aruiz@um.es>
2006-03-23 17:51:07 +0000

    opengl polygon mesh

--- demo.hs
+++ demo.hs
@@ -13,7 +13,7 @@ main = do
     -- let f k = 0.2 * fromIntegral k
     -- plotOpenGL $ \k -> (x, cos x + 0.2 * cos (5*x- f k))
     let f k = 1.0 * fromIntegral k
-    let rg = linspace 50 (-2,2)
+    let rg = linspace 40 (-2,2)
     let (x,y) = meshdom rg rg
     let r2 = x*x+y*y
     meshOpenGL $ \k -> (x,y,exp (-r2) * cos ((fromIntegral k/100 ::Double) <>r2))
@@ -32,11 +32,10 @@ meshOpenGL f = do
         --let sc = 1/(fromIntegral 0 +1)/r :: GLfloat
         -- scale sc sc 0.5
         -- translate $ Vector3 (-r/2::GLfloat) (-r/2) 0
-        
+        polygonMode $= (Line,Line)
         scale (0.5::GLfloat) 0.5 0.5
         rotate (fromIntegral k) $ Vector3 0 1 (0::GLfloat)
         rotate (fromIntegral k) $ Vector3 1 0 (0::GLfloat)
-        
         drawMesh x y z          
     
 -----------------------------------------------------    
@@ -59,18 +58,19 @@ drawPolyline x y = do
 for = flip mapM_           
            
 drawMesh x y z = do 
-    renderPrimitive Points $ 
-        for [0 .. rows z - 1] $ \i -> 
-            for [0 .. cols z - 1] $ \j -> do
+    renderPrimitive Triangles $ 
+        for [1 .. rows z - 1] $ \i -> 
+            for [1 .. cols z - 1] $ \j -> do
                 vertex (Vertex3 (x!!:(i,j)) (y!!:(i,j)) (z!!:(i,j)))
+                vertex (Vertex3 (x!!:(i-1,j)) (y!!:(i-1,j)) (z!!:(i-1,j)))
+                vertex (Vertex3 (x!!:(i,j-1)) (y!!:(i,j-1)) (z!!:(i,j-1)))
                 --vertex (Vertex3 (fromIntegral j) (fromIntegral i) (z!!:(i,j)))
-                --print (i,j,x!!:(i,j))
              
 
 
            
-dibujar f frameNumber = do
-    n <- readIORef frameNumber
+dibujar f state = do
+    (n,_) <- readIORef state
     --clearColor $= Color4 1 0 0 1
     clear [ColorBuffer]
     f n
@@ -83,7 +83,7 @@ prepareOpenGL f = do
     initialDisplayMode $= [DoubleBuffered, WithDepthBuffer]
     createWindow "OpenGL Plot"
     windowSize $= Size 400 400
-    keyboardMouseCallback $= Just keyboard
+    
 
     --lighting $= Enabled
     position (Light 0) $= Vertex4 0 1 (-0.3) 0.1
@@ -91,23 +91,25 @@ prepareOpenGL f = do
     
     --depthFunc $= Just Less
     
-    polygonSmooth $= Enabled
-    lineSmooth $= Enabled
+    --polygonSmooth $= Enabled
+    --lineSmooth $= Enabled
         
-    frameNumber <- newIORef (0::Int) -- the state is just the frame number
-    displayCallback $= dibujar f frameNumber
+    state <- newIORef (0::Int, True) -- the state is the frame number and the pausing mode
+    displayCallback $= dibujar f state
+    keyboardMouseCallback $= Just (keyboard state)
     --idleCallback $= Just (newFrame frameNumber)
-    addTimerCallback 2000 $ newFrame frameNumber
+    addTimerCallback 2000 $ modify state
     mainLoop
-
-oper fun var = modifyIORef var fun
             
-newFrame state = do
-    oper (+1) state
+modify state = do
+    modifyIORef state $ \(n,s) -> if s then (n+1,s) else (n,s)
     postRedisplay Nothing
-    addTimerCallback 30 $ newFrame state
+    addTimerCallback 30 $ modify state
     
-keyboard (Char '\27') _ _ _ = do
+keyboard _ (Char '\27') Down _ _ = do
     exitWith ExitSuccess
+    
+keyboard st (Char 'p') Up _ _ = do
+    modifyIORef st $ \(n,s) -> (n,not s)
 
-keyboard _ _ _ _ = return ()
+keyboard _ _ _ _ _ = return ()
