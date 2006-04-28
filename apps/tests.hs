720339c6331c7141d1da6fab5d3de96b59bea442
Alberto Ruiz <aruiz@um.es>
2006-04-28 11:36:00 +0000

    some corrections

--- apps/tests.hs
+++ apps/tests.hs
@@ -25,23 +25,24 @@ m2 = realMatrix [[1,0.1, 1],
     
 -------------------------------------------------------------------
 
-classifyTest = do
+classifyTest n1 n2 numErr = do
     m <- gslReadMatrix "mnist.txt" (5000,785)
-    let rawproblem = prepareProblem (takeRows 4000 m) (dropRows 4000 m)
+    let rawproblem = prepareProblem (takeRows n1 m) (dropRows (5000-n2) m)
     let st = stat (designSamples rawproblem)
     let codec = pca (ReconstructionQuality 0.8) st
     let problem = preprocessProblem codec rawproblem
     let (f,e,m) = prepareClasif (mahalanobisDist.stat) problem
-    let errors = 1000 - sum (toList (diag m))
-    assertEqual "classifyTest, " 63 errors
-    
+    let errors = (sum.toList.flatten $ m) - (sum.toList.diag $ m)
+    assertEqual ("classifyTest "++show (n1,n2)++", ") numErr errors
+        
 -------------------------------------------------------------------
 
 tests = TestList 
     [ TestCase (assertBool "factorize1" (factorizeCameraTest m1))
     , TestCase (assertBool "factorize2" (factorizeCameraTest m2))
     , TestCase (assertBool "pose"       (poseEstimationTest  m1))
-    , TestCase classifyTest
+    , TestCase $ classifyTest 500 500 129 
+    --, TestCase $ classifyTest 4000 1000 63 
     ]
                  
 main = runTestTT tests
