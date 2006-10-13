eac07fcc1ea2f9d115fa845c7e8c2101c506ce86
Alberto Ruiz <aruiz@um.es>
2006-10-13 16:49:04 +0000

    more UArray functions

--- src/GSL/LinearAlgebra/Matrix.hs
+++ src/GSL/LinearAlgebra/Matrix.hs
@@ -23,9 +23,13 @@ module GSL.LinearAlgebra.Matrix (
     dim, dims, rows, cols, dim', dims', rows', cols',
     trans, trans',
     diag, diag',
-    disp, disp',
+    disp, disp', elems',
     toRows, toCols, fromRows, fromCols,
     toRows', toCols', fromRows', fromCols',
+    fromBlocks, fromBlocks',
+    eye, constantV, constantM,
+    complexV, complexM, partV, partM, conjV, conjM,
+    flatten, reshape, flatten', reshape'
 
 ) where
 
@@ -78,6 +82,8 @@ disp n = putStrLn . showUMatrix " " (shf n)
 disp' :: Int -> UCMatrix -> IO ()
 disp' n = putStrLn . showUCMatrix " | " (shfc n)
 
+elems' :: UCVector -> [Complex Double]
+elems' = cosa . elems
 
 toRows m = map row [r1..r2]
     where ((r1,c1),(r2,c2)) = bounds m
@@ -110,6 +116,12 @@ fromRows' lv = listArray ((0,0,Re),(r-1,c-1,Im)) (concatMap elems lv)
 
 fromCols' :: [UCVector] -> UCMatrix
 fromCols' = trans' . fromRows'
+
+fromBlocks :: [[UMatrix]] -> UMatrix
+fromBlocks = joinVert . map (joinHoriz)
+
+fromBlocks' :: [[UCMatrix]] -> UCMatrix
+fromBlocks' = joinVert' . map (joinHoriz')
 
 
 ----------------------------------------------------------------------
@@ -148,3 +160,69 @@ common f = commonval . map f where
     commonval [] = Nothing
     commonval [a] = Just a
     commonval (a:b:xs) = if a==b then commonval (b:xs) else Nothing
+
+joinVert :: [UMatrix] -> UMatrix
+joinVert = fromRows . concatMap toRows
+
+joinHoriz :: [UMatrix] -> UMatrix
+joinHoriz = fromCols . concatMap toCols
+
+joinVert' :: [UCMatrix] -> UCMatrix
+joinVert' = fromRows' . concatMap toRows'
+
+joinHoriz' :: [UCMatrix] -> UCMatrix
+joinHoriz' = fromCols' . concatMap toCols'
+
+constantV :: Double -> Int -> UVector
+constantV x n = accumArray (+) x (0,n-1) []
+
+constantM :: Double -> (Int,Int) -> UMatrix
+constantM x (r,c) = accumArray (+) x ((0,0),(r-1,c-1)) []
+
+eye :: Int -> UMatrix
+eye n = accumArray (+) 0 ((0,0),(n-1,n-1)) [((k,k),1)|k<-[0..n-1]]
+
+complexV :: Cx -> UVector -> UCVector
+complexV p v = accumArray (+) 0 ((s1,Re),(s2,Im)) [((k,p),v!k)|k<-[s1..s2]]
+    where (s1,s2) = bounds v
+
+complexM :: Cx -> UMatrix -> UCMatrix
+complexM p m = accumArray (+) 0 ((r1,c1,Re),(r2,c2,Im)) [((i,j,p),m!(i,j))|i<-[r1..r2],j<-[c1..c2]]
+    where ((r1,c1),(r2,c2)) = bounds m
+
+partV :: Cx -> UCVector -> UVector
+partV p v = ixmap (s1,s2) (\k->(k,p)) v
+    where ((s1,_),(s2,_)) = bounds v
+
+partM :: Cx -> UCMatrix -> UMatrix
+partM p m = ixmap ((r1,c1),(r2,c2)) (\(i,j)->(i,j,p)) m
+    where ((r1,c1,_),(r2,c2,_)) = bounds m
+
+conjV :: UCVector -> UCVector
+conjV v = array (bounds v) (map f $ assocs v)
+    where f (i@(_,Re),v) = (i,v)
+          f (i@(_,Im),v) = (i,-v)
+
+conjM :: UCMatrix -> UCMatrix
+conjM m = array (bounds m) (map f $ assocs m)
+    where f (i@(_,_,Re),v) = (i,v)
+          f (i@(_,_,Im),v) = (i,-v)
+
+gflatten :: Ix i => UArray i Double -> UVector
+gflatten a = listArray (0,length l-1) l
+    where l = elems a
+
+flatten:: UMatrix -> UVector
+flatten = gflatten
+
+flatten':: UCMatrix -> UCVector
+flatten' m = listArray ((0,Re),((length l `quot` 2)-1,Im)) l
+    where l = elems m
+
+reshape :: (Int,Int) -> UVector -> UMatrix
+reshape (r,c) v | l <- elems v, r*c == (length l) = listArray ((0,0),(r-1,c-1)) l
+                | otherwise = error "reshape"
+
+reshape' :: (Int,Int) -> UCVector -> UCMatrix
+reshape' (r,c) v | l <- elems v, r*c == (length l `quot` 2) = listArray ((0,0,Re),(r-1,c-1,Im)) l
+                 | otherwise = error "reshape'"
