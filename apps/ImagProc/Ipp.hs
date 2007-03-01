f02dfeaa10fcabfd021b01631c35eebcaffd54e8
Alberto Ruiz <aruiz@um.es>
2007-03-01 16:53:05 +0000

    the forgotten ImagProc.Ipp.hs

---
+++ apps/ImagProc/Ipp.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS  #-}
+-----------------------------------------------------------------------------
+{- |
+Module      :  ImagProc.Ipp
+Copyright   :  (c) Alberto Ruiz 2006
+License     :  GPL-style
+
+Maintainer  :  Alberto Ruiz (aruiz at um dot es)
+Stability   :  very provisional
+Portability :  hmm...
+
+Experimental interface to Intel's IPP image processing libraries
+
+-}
+-----------------------------------------------------------------------------
+
+module ImagProc.Ipp (
+    module ImagProc.Ipp.Core,
+    module ImagProc.Ipp.Wrappers,
+) where
+
+import ImagProc.Ipp.Core
+import ImagProc.Ipp.Wrappers
