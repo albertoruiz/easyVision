b21e267d96ccb1ce19ccced4352c33fb52dcf908
Alberto Ruiz <aruiz@um.es>
2007-06-30 10:38:57 +0000

    more trifocal

--- examples/trifocal.hs
+++ examples/trifocal.hs
@@ -1,11 +1,10 @@
---{-# OPTIONS_GHC -fglasgow-exts #-}
-
 import Vision
 import Data.Packed.Tensor
 import Data.Packed.Internal.Tensor
 import Data.Packed.Internal.Matrix
 import GSL.Vector
 import GSL
+import Data.List(sort)
 
 -- some defs
 
@@ -33,11 +32,11 @@ chop n = if abs n < 1E-10 then 0 else n
 -- cameras
 
 m1 = syntheticCamera $ easyCamera 40 (0,0,0) (0,0,1) 0
-cen1 = tvector "c" $ nullspaceOK1 m1
-c1 = tensorFromMatrix (Contravariant,"m1") (Covariant, "m2") m1
+cen1 = tvector (nullspaceOK1 m1) ! "c"
+c1 = tensorFromMatrix Contravariant Covariant m1
 
 m2 = syntheticCamera $ easyCamera 20 (1,0,0) (0.5,0.5,1) 0
-c2 = tensorFromMatrix (Contravariant,"n1") (Covariant, "n2") m2
+c2 = tensorFromMatrix Contravariant Covariant m2
 
 f = fundamentalFromCameras m1 m2
 
@@ -45,33 +44,49 @@ eps3 = leviCivita 3
 eps4 = raise $ leviCivita 4
 tf = eps3!"pqr" * eps4!"bcij" * c1!"pb" * c1!"qc" * c2!"si" * c2!"tj" * eps3!"stk"
 
+
+m3 = syntheticCamera $ easyCamera 30 (0,1,0) (0.5,1,2) 20
+c3 = tensorFromMatrix Contravariant Covariant m3
+
+tri = eps4!"abcd" * c1!"ia" * c2!"jb" * c3!"pc" * c3!"qd" * eps3!"pqk"
+
+m4 = syntheticCamera $ easyCamera 45 (1,1,-1) (2,1,0) (-30)
+c4 = tensorFromMatrix Contravariant Covariant m3
+
+qua = eps4!"abcd" * c1!"ia" * c2!"jb" * c3!"kc" * c4!"rd"
+
+
+line = tvector (vector [1,2,3,1]) !"i" /\ tvector (vector [5,-4,3,1]) !"j"
+il1 = eps3!"abp" * line * c1!"ai" * c1!"bj"
+il2 = dual (line * c2!"ai" * c2!"bj") !"q"
+il3 = eps3!"abr" * line * c3!"ai" * c3!"bj"
+
+point = tvector (vector [2,5,7,1])
+ip1 = point!"i" * c1!"ji"
+ip2 = point!"i" * c2!"ji"
+ip3 = point!"i" * c3!"ji"
+ip4 = point!"i" * c4!"ji"
+
 -- plane induced by reprojection of line joining two image points
 p1 = vector [0,0.1,1]
 p2 = vector [1,0.1,1]
 l12 = cross p1 p2
 plane1 = l12 <> m1
 
-
-tp1 = tvector "p" p1
+tp1 = tvector p1
+tp2 = tvector p2
 
 -- alternative tensor computation
 
-tP1 = tvector "p" $ pinv m1 <> p1
-tP2 = tvector "q" $ pinv m1 <> p2
+tP1 = tvector (pinv m1 <> p1)
+tP2 = tvector (pinv m1 <> p2)
 
 plane1span = cen1 /\ tP1 /\ tP2
 
 unitaryT = liftTensor unitary
 
-
-ray1 = tvector "c" (vector [0,0,0,1]) /\ tvector "p" (pinv m1 <> p1)
-
-pl1 = tvector "q1" $ vector [0,-(p1@>2),p1@>1] <> m1
-pl2 = tvector "q2" $ vector [-(p1@>2),0,p1@>0] <> m1
-
-infixl 7 -:
-n -: t = T d (cdat m)
-    where (d,m) = putFirstIdx n t
+pl1 = tcovector $ vector [0,-(p1@>2),p1@>1] <> m1
+pl2 = tcovector $ vector [-(p1@>2),0,p1@>0] <> m1
 
 infixl 8 @@
 t @@ l = withIdx t l
@@ -79,24 +94,66 @@ t @@ l = withIdx t l
 infixl 8 !
 t ! l = withIdx t (map return l)
 
+infixl 5 /\
+a /\ b = wedge a b
 
 infixl 5 |*|
-t1 |*| t2 = contractionF t1 t2
+a |*| b = rawProduct a b
 
+
+m3x3 = (3><3) [0.5,1,0,0,2,0,7,0,1::Double]
+m4x4 = (3><3) [0.5,1,0,0,2,0,7,0,1::Double] <|> fromList [2,2,1::Double]
+                             <-> fromList [2,0,0,2::Double]
+
+y1 = tvector (vector [1,2,3,4]) ! "p"
+y2 = tvector (vector [3,2,0,7]) ! "q"
+
+tr4 l = T d (fromList $ take (4^4) l) where
+    d = zipWith3 IdxDesc (repeat 4) (repeat Covariant) (take 4 seqind)
 
 main = do
+    putStrLn "Plane reprojection"
     print $ unitary plane1
-    print $ unitaryT $ dualMV $ plane1span
-    print $ niceAS plane1span
-    print $ niceAS $ dualMV $ plane1span
-    print $ dualMV $ plane1span
+    print $ unitaryT $ dual (tp1/\tp2) !"k" * c1!"kj"
+    print $ unitaryT $ dual $ plane1span
+    print $ niceAS $ unitaryT $ dual plane1span
+    print $ niceAS $ unitaryT $ plane1span
+    print $ niceAS $ unitaryT $ codual $ dual (tp1/\tp2) !"k" * c1!"kj"
     putStrLn "-----------------"
     putStrLn "point reprojection without pseudoinverse"
-    print $ unitaryT $ dualMV $ raise $ dualMV tp1 |*| "m1" -: c1 |*| "m1" -: c1
     print $ unitaryT $ cen1 /\ tP1
+    print $ unitaryT $ codual $ dual tp1 !"ab" * c1!"ai" * c1!"bj"
+    print $ unitaryT $ eps4!"ijkr" * eps3!"pab" * tp1 !"p" * c1!"ai" * c1!"bj"
+    print $ unitaryT $ eps4!"ijkr" * pl1!"i" * pl2!"j"
+    -- reproject two planes <=> reproject the dual, eps gives the lines...
     putStrLn "-----------------"
     putStrLn "Fundamental matrix"
     print $ chopT tf
     print $ chopM $ normat $ reshape 3 $ ten tf
     print $ chopM $ normat $ f
-
+    putStrLn "-----------------"
+    putStrLn "Trifocal tensor"
+    print $ chopT tri
+    print $ unitaryT il3
+    print $ unitaryT $ tri!"kij" * il1!"i" * il2!"j"
+    putStrLn "-----------------"
+    putStrLn "Cuadrifocal tensor"
+    print $ chopT qua
+    print $ chopT $ qua!"pqrs" * eps3!"pib" * ip1!"i" * eps3!"qjc" * ip2!"j" * eps3!"rkd" * ip3!"k" * eps3!"sle" * ip4!"l"
+    putStrLn "-----------------"
+    putStrLn "Inverse"
+    let m = tensorFromMatrix Contravariant Covariant m3x3
+    print (2 * inv m3x3)
+    print $ eps3!"iab" * m!"ap" * m!"bq" * raise eps3 ! "pqs"
+    let m = tensorFromMatrix Contravariant Covariant m4x4
+    print (12 * inv m4x4)
+    print $ tridx ["s","i"] $ leviCivita 4!"iabc" * m!"ap" * m!"bq" * m! "cr" * raise (leviCivita 4) ! "pqrs"
+    putStrLn "-----------------"
+    putStrLn "exterior product properties"
+    print $ leviCivita 4 !"pqrs" * (y1 /\ y2)
+    print $ 2 * leviCivita 4 !"pqrs" *  y1  * y2
+    let t = tr4 [1::Double .. ]
+    print $ t !"pqrs" * (y1 /\ y2)
+    print $ t !"pqrs" *  y1  * y2
+    print $ y1 /\ y2
+    print $ y1!"i" * y2!"j" * scalar 0.5 * leviCivita 4 !"ijab" * raise (leviCivita 4) !"abrs"
