743d50ed968e81b57fca7004502535c6876c6e0c
Alberto Ruiz <aruiz@um.es>
2008-01-11 20:19:04 +0000

    point tracker and demostereo

--- lib/EasyVision/MiniApps.hs
+++ lib/EasyVision/MiniApps.hs
@@ -18,11 +18,13 @@ module EasyVision.MiniApps (
     getCatalog,
     catalogBrowser,
     hsvPalette,
-    scatterWindow
+    scatterWindow,
+    regionDetector, regionTracker
 )where
 
-import Graphics.UI.GLUT as GL hiding (Size,Point)
-import EasyVision.GUI
+import Graphics.UI.GLUT as GL hiding (Size,Point,Matrix,matrix)
+import EasyVision.GUI hiding (State)
+import EasyVision.Parameters
 import ImagProc
 import Data.List(transpose)
 import Control.Monad(when)
@@ -30,10 +32,11 @@ import ImagProc.Ipp.Core
 import Foreign.C.Types(CUChar)
 import Foreign
 import qualified Data.Map as Map
-import Data.List(sort,nub)
+import Data.List(sort,nub,sortBy)
 import EasyVision.Util
 import Numeric.LinearAlgebra
 import Classifier.Base(group)
+import Data.IORef
 
 -- | reads a labeled video
 readCatalog :: String -> Size -> String -> Maybe Int -> (ImageYUV-> a) -> IO [(a,String)]
@@ -208,3 +211,133 @@ scatterWindow name sz exs coor  = do
             rdesi $= ((i-1) `mod` n,j)
             postRedisplay Nothing
         kbd _ a b c d = kbdQuit a b c d
+
+-----------------------------------------------------------------------
+
+getIntegral o name = fromIntegral `fmap` (getParam o name :: IO Int)
+
+regionDetector :: String -> IO Channels -> IO (IO (Channels, Maybe (Point,[Point])))
+regionDetector "" cam = do
+    o <- createParameters [("kb",intParam 60  0 255),
+                           ("kg",intParam 100 0 255),
+                           ("kw",intParam 200 0 255),
+                           ("c1",intParam 7 1 9),
+                           ("c2",intParam 8 1 9)]
+    return $ do
+        kb <- getIntegral o "kb"
+        kg <- getIntegral o "kg"
+        kw <- getIntegral o "kw"
+        c1 <- getIntegral o "c1"
+        c2 <- getIntegral o "c2"
+        orig <- cam
+        let col = detectRange c1 c2 . hsvCode kb kg kw . hsv $ orig
+            rawconts = map (pixelsToPoints (size col). fst3) $
+                       sortBy (compare `on` (negate.fst2)) $
+                       contours 5 200 1 True col
+            conts = map momentsContour rawconts
+            p = case conts of
+                    [] -> Nothing
+                    (x,y,_,_,_):_ -> Just (Point x y, head rawconts)
+        return (orig, p)
+
+
+regionDetector name cam = do
+    e <- evWindow () name (mpSize 8) Nothing (const kbdQuit)
+    cam' <- regionDetector "" cam
+    return $ do
+        (orig,pc) <- cam'
+        inWin e $ do
+            drawImage (rgb orig)
+            setColor 1 1 1
+            pointSize $= 5
+            pointCoordinates (size (rgb orig))
+            case pc of
+                Just (p,cont) -> do
+                    renderPrimitive Points $ mapM_ vertex [p]
+                    renderPrimitive LineLoop $ mapM_ vertex cont
+                Nothing -> return ()
+        return (orig, pc)
+
+detectRange a b im = purifyWith (set8u 0) $
+    thresholdVal8u a 0 IppCmpLess im >>=
+    thresholdVal8u b 0 IppCmpGreater
+
+fst3 (a,_,_) = a
+fst2 (_,a,_) = a
+
+----------------------------------------------------------------
+
+-- | to do
+regionTracker :: String -> IO (Channels,Maybe (Point,[Point])) -> IO (IO (Channels, (Point,(Double,Double))))
+regionTracker "" detector = do
+    r <- newIORef s0
+    return $ do
+        (orig,p) <- detector
+        st <- get r
+        let st'@(State x c) =
+                case p of
+                    Nothing -> blind sys st
+                    Just (Point x y,_)  -> kalman sys st (vector [x, y])
+        r $= st'
+        let pt = Point (x@>0) (x@>1)
+            v = (x@>2,x@>3)
+        return (orig, (pt,v))
+
+regionTracker name detector = do
+    det <- regionTracker "" detector
+    e <- evWindow () "kalman" (mpSize 8) Nothing (const kbdQuit)
+    return $ do
+        (orig, (pt@(Point x y),v@(vx,vy))) <- det
+        let pt2 = Point (x+vx) (y+vy)
+        inWin e $ do
+            drawImage (rgb orig)
+            setColor 1 1 1
+            pointSize $= 5
+            pointCoordinates (size (rgb orig))
+            renderPrimitive Points $ mapM_ vertex [pt]
+            setColor 1 0 0
+            lineWidth $= 2
+            renderPrimitive Lines $ mapM_ vertex [pt,pt2]
+        return (orig, (pt,v))
+
+-- Kalman filter for 2D position and velocity
+
+vector l = fromList l :: Vector Double
+matrix ls = fromLists ls :: Matrix Double
+diagl = diag . vector
+
+f = matrix [[1,0,1,0],
+            [0,1,0,1],
+            [0,0,1,0],
+            [0,0,0,1]]
+
+h = matrix [[1,0,0,0],
+            [0,1,0,0]]
+
+q = 1 * diagl [1,1,1,1]
+
+r = 10 * diagl [1,1]
+
+s0 = State (vector [0, 0, 0, 0]) (diagl [1, 1, 1, 1])
+
+sys = System f h q r
+
+data System = System {kF, kH, kQ, kR :: Matrix Double}
+data State = State {sX :: Vector Double , sP :: Matrix Double} deriving Show
+type Measurement = Vector Double
+
+kalman :: System -> State -> Measurement -> State
+kalman (System f h q r) (State x p) z = State x' p' where
+    px = f <> x                            -- prediction
+    pq = f <> p <> trans f + q             -- its covariance
+    y  = z - h <> px                       -- residue
+    cy = h <> pq <> trans h + r            -- its covariance
+    k  = pq <> trans h <> inv cy           -- kalman gain
+    x' = px + k <> y                       -- new state
+    p' = (ident (dim x) - k <> h) <> pq    -- its covariance
+
+
+blind :: System -> State -> State
+blind (System f h q r) (State x p) = State x' p' where
+    x' = f <> x
+    p' = f <> p <> trans f + q
