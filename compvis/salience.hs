666a14723ae4c29cf6ea7ac7e0cc628ccc6b1518
Alberto Ruiz <aruiz@um.es>
2008-05-16 09:34:31 +0000

    save work

---
+++ compvis/salience.hs
@@ -0,0 +1,84 @@
+--experiment...
+
+import EasyVision
+--import ImagProc.Ipp.Core(intersection)
+import Graphics.UI.GLUT hiding (RGB,Size,minmax,histogram,Point,set)
+import GHC.Float(float2Double,double2Float)
+--import Text.Printf
+import Control.Monad
+--import Data.List(transpose)
+
+
+------------------------------------------------------------
+
+main = do
+
+    prepare
+
+    sz <- findSize
+
+    (cam, ctrl)  <- getCam 0 sz  >>= withChannels >>= withPause
+
+    w <- evWindow () "image" sz Nothing  (const (kbdcam ctrl))
+    w2 <- evWindow () "pru" sz Nothing  (const (kbdcam ctrl))
+
+    o <- createParameters [("rad"  ,intParam 10 1 30),
+                           ("sigma",realParam 6 0.1 10),
+                           ("sigma2",realParam 6 0.1 20),
+                           ("thres",realParam 0.4 0 1),
+                           ("n",intParam 4 0 20),
+                           ("what",intParam 0 0 1)]
+
+
+    launch $ do
+        orig <- cam
+        th <- getParam o "thres"
+        rad <- getParam o "rad"
+        sigma <- getParam o "sigma"
+        sigma2 <- getParam o "sigma2"
+        --nmax <- getParam o "n"
+        what <- getParam o "what" :: IO Int
+        let img = (gaussS sigma2
+                  . sqrt32f
+                  . abs32f
+                  -- . thresholdVal32f 0 0 IppCmpGreater
+                  . hessian
+                  . secondOrder
+                  . gaussS sigma
+                  . float . gray) orig
+            s = sum32f img
+            mx = double2Float $ 10*s/fromIntegral (h*w) where (Size h w) = sz
+            imgreg = thresholdVal32f (mx*th) 1 IppCmpGreater $ thresholdVal32f (mx*th) 0 IppCmpLess $ img
+
+            area = 1
+            fracpix = 2
+            pixarea = h*w*area`div`10000 where (Size h w) = sz
+            redu = douglasPeuckerClosed fracpix
+            cs = map (redu.fst3) $ contours 100 pixarea 128 True (toGray imgreg)
+
+        inWin w2 $ do
+            drawImage imgreg
+
+        inWin w $ do
+            let pts = getPoints32f 200 . thresholdVal32f (mx*th) 0 IppCmpLess . localMax (2*rad+1) $ img
+            if what == 0
+                then drawImage (gray $ orig)
+                else drawImage (autoscale img)
+            pointCoordinates sz
+            pointSize $= 5
+            setColor 1 0 0
+            when (not (null pts)) $ do
+                renderPrimitive Points (mapM_ vertex (pixelsToPoints sz pts))
+            --pixelCoordinates sz
+            --setColor 1 0 0
+            --lineWidth $= 2
+            --mapM_ shcont cs
+
+fst3 (a,_,_) = a
+
+shcont c = do
+    renderPrimitive LineLoop $ mapM_ vertex c
+
+autoscale im = f im
+    where (mn,mx) = minmax im
+          f = if mn == mx then scale32f8u 0 1 else scale32f8u mn mx
---
+++ examples/pls.hs
@@ -0,0 +1,124 @@
+import Numeric.LinearAlgebra
+import Util.Stat
+import Debug.Trace
+import Text.Printf
+import Numeric.LinearAlgebra.LAPACK
+import System.Random
+import Data.List(sortBy)
+import Data.Function(on)
+
+debug x = trace (show x) x
+
+disp m = putStrLn . format " " (printf "%.3f") $ m
+
+vector x = Numeric.LinearAlgebra.fromList x :: Vector Double
+
+lat = [[l1,l2,l3] | l1<- dom, l2 <- dom, l3 <- dom]
+    where dom = [-5..5]
+
+gx [a,b,c] = [a+b,a-b+c,3*a-2*b-c,5*a+b+2*c,a-3*b-3*c]
+gy [a,b,c] = [2*a-4*b,-a+2*b,3*a+10*b]
+
+x = addNoise 100 3 $ fromLists $ map gx lat
+y = addNoise 200 3 $ fromLists $ map gy lat
+
+addNoise :: Int -> Double -> Matrix Double -> Matrix Double
+addNoise seed sz m = m + res where
+    r = rows m
+    c = cols m
+    res = reshape c . fromList . take (r*c) $ randomRs (-sz, sz::Double) (mkStdGen seed)
+
+scramble seed l = map fst $ sortBy (compare `on` snd) randomTuples where
+    randomTuples = zip l (randomRs (0, 1::Double) (mkStdGen seed))
+
+prob = scramble 17 $ zip (map toList $ toRows x) (map toList $ toRows y)
+
+unitary v = recip (norm v) .* v
+
+norm :: Vector Double -> Double
+norm = pnorm PNorm2
+
+normalize = normalizedData . stat
+
+mean l = sum l / fromIntegral (length l)
+
+---------------------------------------------
+
+type Learner a b = [(a,b)] -> (a -> b)
+
+type Vec = Vector Double
+type Mat = Matrix Double
+
+normalizer :: [Vec] -> (Mat, Vec -> Vec, Vec -> Vec)
+normalizer vs = (x,f,g) where
+    s = stat (fromRows vs)
+    m = meanVector s
+    d = sqrt (varianceVector s)
+    f x = (x-m)/d
+    g x = m + x*d
+    x = normalizedData s
+
+withNormalized method prob = gy . method nx ny . fx
+    where
+    (xs,ys) = unzip prob
+    (nx,fx,_) = normalizer (map vector xs)
+    (ny,_,gy) = normalizer (map vector ys)
+
+
+mlr xc yc = (<>m)
+    where m = linearSolveSVD xc yc
+
+---------------------------------------------
+
+mlrReduced n xc yc = (<>q) . (<>w)
+    where m = linearSolveSVD xc yc
+          (u,_,_) = svd m
+          w = takeColumns n u
+          q = linearSolveSVD ((xc <> w)) yc
+
+----------------------------------------------
+
+pls n xc yc = (<>q) . (<>w)
+    where
+    w = takeColumns n v
+    (_,_,v) = svd (trans yc <> xc)
+    t = xc <> w
+    q = linearSolveSVD t yc
+
+
+----------------------------------------------
+
+pcr n xc yc = (<>q) . (<>w) where
+    w = takeColumns n v
+    (_,v) = eigSH (trans xc <> xc)
+    t = xc <> w
+    q = linearSolveSVD t yc
+
+----------------------------------------------
+
+mse f prob = mean [ norm (vector y - f (vector x) ) ^ 2 | (x,y) <- prob ]
+
+mseN f prob = k * sqrt (mse f prob)
+    where k = recip $ fromIntegral $ length (snd (head prob))
+
+rotate n xs = drop n xs ++ take n xs
+
+-- leave n out
+lno n cost method prob = mean cases where
+    work (test,prob) = cost (method prob) test
+    cases = map (work . splitAt n . flip rotate prob ) [0, n .. (length prob - n)]
+
+----------------------------------------------
+
+line msg = putStrLn $ replicate 5 '-' ++ msg ++ replicate 30 '-'
+
+for = flip mapM_
+
+main = do
+    print $ mseN (withNormalized mlr prob) prob
+    line "mlr"
+    for [1..5] $ \n -> printf "%d: %.3f\n" n $ lno 10 mseN (withNormalized (mlrReduced n)) prob
+    line "pls"
+    for [1..5] $ \n -> printf "%d: %.3f\n" n $ lno 10 mseN (withNormalized (pls n)) prob
+    line "pcr"
+    for [1..5] $ \n -> printf "%d: %.3f\n" n $ lno 10 mseN (withNormalized (pcr n)) prob
