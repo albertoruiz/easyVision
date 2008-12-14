05571696b99549513dab87628fbd267347a9f6df
Alberto Ruiz <aruiz@um.es>
2008-12-14 13:31:46 +0000

    ellipse tangents

--- compvis/pose/ellipses.hs
+++ compvis/pose/ellipses.hs
@@ -13,10 +13,9 @@ import Vision.Geometry
 import Vision.Camera
 import Numeric.GSL.Polynomials
 import Text.Printf
+import Data.Maybe(isJust)
 
-------------------------------------------------------------
-
-sz' = Size 640 480
+sz' = Size 600 600
 
 main = do
 
@@ -30,8 +29,7 @@ main = do
                            ("area",percent 5),
                            ("tolerance",percent 10),
                            ("fracpix",realParam 0.8 0 10),
-                           ("scale", realParam 1 0 5),
-                           ("method", intParam 1 1 3)]
+                           ("scale", realParam 1 0 5)]
 
     w  <- evWindow () "Ellipses" sz  Nothing (const (kbdcam ctrl))
     wr <- evWindow () "Rectif"   sz' Nothing (const (kbdcam ctrl))
@@ -47,7 +45,6 @@ worker w wr cam param = do
     fracpix <- getParam param "fracpix"
     tol <- getParam param "tolerance"
     sc  <- getParam param "scale"
-    method  <- getParam param "method" :: IO Int
 
     orig <- cam
 
@@ -60,74 +57,75 @@ worker w wr cam param = do
             cs1 = map (redu.fst3) $ contours 100 pixarea th2 True (gray orig)
             cs2 = map (redu.fst3) $ contours 100 pixarea th2 False (gray orig)
             candidates = cs1++cs2 -- ++[control]
-            rawellipses = filter (isEllipse tol) candidates
-            ellipses = sortBy (compare `on` (negate.areaConic)) $ map analyzeConic rawellipses
-            okellipses = sortBy (compare `on` (negate.perimeter)) rawellipses
+            rawellipses = sortBy (compare `on` (negate.perimeter))
+                        . filter (isEllipse tol)
+                        $ candidates
             est (Closed ps) = fst $ estimateConicRaw ps
-            okellip = map est okellipses
+            ellipMat = map est rawellipses
+            ellipses = map (fst.analyzeEllipse) ellipMat
         pointCoordinates (size $ gray orig)
         lineWidth $= 1
         setColor' Col.yellow
         mapM_ shcont candidates
         setColor' Col.red
         lineWidth $= 3
-        --mapM_ (disppl . conicPoints) ellipses
-        mapM_ (disppl . conicPoints. fst .analyzeEllipse) okellip
-        mapM_ (disppl . conicMajor. fst . analyzeEllipse) okellip
-        lineWidth $= 1
-        setColor' Col.gray
-        mapM_ shcont okellipses
+        mapM_ (disppl . conicPoints) ellipses
         when (length ellipses >= 2) $ do
-            -- print $ analyzeEllipse (head $ okellip)
-            let (w, coeffs) = case method of
-                    1 -> reduceConics (okellip!!0) (okellip!!1)
-                    2 -> reduceConics' (okellip!!0) (okellip!!1)
-                    3 -> reduceConicsEmpirical (ellipses!!0) (ellipses!!1)
-                -- coeffs' = (-1,-16,8,11/4)
-                sol = htc (comp $ inv w) $ intersectionReduced coeffs
-                (ij,other) = selectSol (ellipses!!0) (ellipses!!1) sol
-                [h1,h2] = getHorizs [ij,other]
-            lineWidth $= 1
-            setColor' Col.green
-            mapM_ (disppl. ht (scaling 0.2<>w). conicPoints) ellipses
-            --print sol'
-            setColor' Col.blue
-            shLine h1
-            setColor' Col.yellow
-            shLine h2
-            pointSize $= 5
-            setColor' Col.purple
-            renderPrimitive Points $ mapM (vertex.map realPart) [ij,other]
-            let recraw = diagl [1,-1,1] <> rectifierFromCircularPoint ij
-            let rec' = recraw
-                (mx,my,_,_,_) = ellipses!!0
-                (mx2,my2,_,_,_) = ellipses!!1
-                [[mx',my'],[mx'2,my'2]] = ht recraw [[mx,my],[mx2,my2]]
-                okrec = similarFrom2Points [mx',my'] [mx'2,my'2] [0,0] [-0.5, 0] <> recraw
-            inWin wr $ do
-                --drawImage $ warp 0 sz' (scaling sc <> w) (gray orig)
-                drawImage $ warp (0,0,0) sz' (scaling sc <> okrec ) (rgb orig)
-                --text2D 30 30 $ show $ focalFromHomogZ0 (inv recraw)
-                -- it is also encoded in the circular points
-                text2D 30 30 $ printf "f = %.2f" $ focalFromCircularPoint (fromList ij)
-                text2D 30 50 $ printf "ang = %.1f" $ abs ((acos $ orthoTest ij)/degree - 90)
+            let sol = intersectionEllipses (ellipMat!!0) (ellipMat!!1)
+                (mbij,mbother) = selectSol (ellipses!!0) (ellipses!!1) sol
+            when (isJust mbij && isJust mbother) $ do
+                let Just ij    = mbij
+                    Just other = mbother
+                    [h1,h2] = getHorizs [ij,other]
+                lineWidth $= 1
+                setColor' Col.blue
+                shLine h1
+                setColor' Col.yellow
+                shLine h2
+                pointSize $= 5
+                setColor' Col.purple
+                renderPrimitive Points $ mapM (vertex.map realPart) [ij,other]
+                setColor' Col.green
+                mapM_ shLine $ map (map realPart) $ tangentEllipses (ellipMat!!0) (ellipMat!!1)
+                let recraw = diagl [1,-1,1] <> rectifierFromCircularPoint ij
+                    (mx,my,_,_,_) = ellipses!!0
+                    (mx2,my2,_,_,_) = ellipses!!1
+                    [[mx',my'],[mx'2,my'2]] = ht recraw [[mx,my],[mx2,my2]]
+                    okrec = similarFrom2Points [mx',my'] [mx'2,my'2] [0,0] [-0.5, 0] <> recraw
+                inWin wr $ do
+                    --drawImage $ warp 0 sz' (scaling sc <> w) (gray orig)
+                    drawImage $ warp (0,0,0) sz' (scaling sc <> okrec ) (rgb orig)
+                    --text2D 30 30 $ show $ focalFromHomogZ0 (inv recraw)
+                    -- it is also encoded in the circular points
+                    text2D 30 30 $ printf "f = %.2f" $ focalFromCircularPoint (fromList ij)
+                    text2D 30 50 $ printf "ang = %.1f" $ abs ((acos $ orthoTest ij)/degree - 90)
 
 
 norm x = pnorm PNorm2 x
 
+mt m = trans (inv m)
+
 fst3 (a,_,_) = a
 
-shcont (Closed c) = do
-    renderPrimitive LineLoop $ mapM_ vertex c
+htp h (Closed c) = Closed . map l2p . ht h . map p2l $ c
+p2l (Point x y) = [x,y]
+l2p [x,y] = Point x y
 
-shcontP (Closed c) = do
-    renderPrimitive LineLoop $ mapM_ vertex c
-    renderPrimitive Points $ mapM_ vertex c
-    pointSize $= 5
-    renderPrimitive Points $ vertex (head c)
+diagl = diag . fromList
 
-isEllipse' tol (Closed ps) = s < fromIntegral (tol::Int)/100000
-    where (_,s) = estimateConicRaw ps
+disppl l = renderPrimitive LineLoop $ mapM_ vertex l
+
+shcont (Closed c) = disppl c
+
+shLine [a,b,c] = renderPrimitive Lines $ mapM f [-1,1]
+    where f x = vertex $ Point x ((-a*x-c)/b)
+
+conicPoints (mx,my,d1,d2,a) = xs where
+    xs = map pt ts
+    ts = tail $ toList $ linspace 50 (0,2*pi)
+    pt t = [mx + x*cos a - y*sin a, my + x*sin a + y*cos a]
+        where x = d1*cos t
+              y = d2*sin t
 
 isEllipse tol c = (ft-f1)/ft < fromIntegral (tol::Int)/1000 where
     wc = whitenContour c
@@ -135,7 +133,6 @@ isEllipse tol c = (ft-f1)/ft < fromIntegral (tol::Int)/1000 where
     f0 = magnitude (f 0)
     f1 = sqrt (magnitude (f (-1)) ^2 + magnitude (f 1) ^2)
     ft = sqrt (norm2Cont wc - f0 ^2)
-
 
 estimateConicRaw ::  [Point] -> (Matrix Double, Double)
 estimateConicRaw ps = (con,err) where
@@ -147,43 +144,7 @@ estimateConicRaw ps = (con,err) where
     eqs = map eq ps
     eq (Point x y) = [x*x, y*y, 2*x*y, 2*x, 2*y, 1.0]
 
-
-analyzeConic (Closed ps) = (mx,my,d1,d2,a) where
-    (mx,my,cxx,cyy,cxy) = momentsContour ps
-    (l1,l2,a) = eig2x2Dir (cxx,cyy,cxy)
-    d1 = 2*sqrt l1
-    d2 = 2*sqrt l2
-
-areaConic (_,_,d1,d2,_) = pi*d1*d2
-
-whitenEllipse (mx,my,d1,d2,a) = diag (fromList [1/d1,1/d2,1]) <> rot3 (a) <> desp (-mx,-my)
-
-disppl l = renderPrimitive LineLoop $ mapM_ vertex l
-
-conicPoints (mx,my,d1,d2,a) = xs where
-    xs = map pt ts
-    ts = tail $ toList $ linspace 50 (0,2*pi)
-    pt t = [mx + x*cos a - y*sin a, my + x*sin a + y*cos a]
-        where x = d1*cos t
-              y = d2*sin t
-
-conicMajor (mx,my,d1,d2,a) = 
-    [[mx,my],[mx+d1*cos a,my+d1*sin a]]
-
-chooseRot mx my a = w where
-    v = fromList [mx,my,1]
-    [mx',my',_] = toList $ rot3 a <> v
-    w = if abs mx' > abs my'
-        then rot3 a
-        else rot3 (a+pi/2)
-
-htp h (Closed c) = Closed . map l2p . ht h . map p2l $ c
-p2l (Point x y) = [x,y]
-l2p [x,y] = Point x y
-
-diagl = diag . fromList
-
--------------------------------------------------------------------
+----------------------------------------------------------------
 
 -- computes a similar 2D homogeneous transformation with moves 2 points to a desired position
 similarFrom2Points [ax,ay] [bx,by] [a'x,a'y] [b'x,b'y] = t where
@@ -195,8 +156,6 @@ similarFrom2Points [ax,ay] [bx,by] [a'x,a'y] [b'x,b'y] = t where
 
 -------------------------------------------------------------------
 
-mt m = trans (inv m)
-
 analyzeEllipse m = ((mx,my,dx,dy,alpha),t) where
     a = m@@>(0,0)
     b = m@@>(1,1)
@@ -206,7 +165,8 @@ analyzeEllipse m = ((mx,my,dx,dy,alpha),t) where
     m1 = t1 <> m <> trans t1
     a' = m1@@>(0,0)
     b' = m1@@>(1,1)
-    t2 = diagl [sqrt (a'/b'), 1,1]
+    t2 = diagl [sqrt (abs $ a'/b'), 1,1]
+    -- abs is necessary for projective reduceConics
     m2' = mt t2 <> m1 <> inv t2
     m2 = m2' */ m2'@@>(0,0)
     d'' = m2@@>(0,2)
@@ -223,35 +183,33 @@ analyzeEllipse m = ((mx,my,dx,dy,alpha),t) where
         else (sy,sx,-phi-pi/2)
 
 -- moves two ellipses to reduced form for easy intersection
-reduceConics c1 c2 = (w, coeffs) where
+-- projective version, required for dual conics (tangents)
+reduceConics c1 c2 = (w, c2') where
+    (s,v) = eigSH' c1
+    sg = signum s
+    p = (if sg@>1 < 0 then flipud else id) (ident 3)
+    w1 = mt $ p <> diag (sg / sqrt (abs s)) <> trans v
+    (mx,my,_,_,a) = fst $ analyzeEllipse (mt w1 <> c2 <> inv w1)
+    w2 = chooseRot mx my a
+    w = w2 <> w1
+    c2' = mt w <> c2 <> inv w
+
+-- affine version
+reduceConics' c1 c2 = (w, c2') where
     w1 = snd $ analyzeEllipse c1
     (mx,my,_,_,a) = fst $ analyzeEllipse (mt w1 <> c2 <> inv w1)
     w2 = chooseRot mx my a
     w = w2 <> w1
-    coeffs = getCoeffs (mt w <> c2 <> inv w)
+    c2' = mt w <> c2 <> inv w
 
+chooseRot mx my a = w where
+    v = fromList [mx,my,1]
+    [mx',my',_] = toList $ rot3 a <> v
+    w = if abs mx' > abs my'
+        then rot3 a
+        else rot3 (a+pi/2)
 
-reduceConics' c1 c2 = (trans $ inv w, coeffs) where
-    (s,v) = eigSH c1
-    sg = signum s
-    t1 = p <> diag (sg / sqrt (abs s)) <> trans v
-    c2' = t1 <> c2 <> trans t1
-    a = c2'@@>(0,0)
-    b = c2'@@>(1,1)
-    c = c2'@@>(0,1)
-    phi = 0.5 * atan2 (2*c) (b-a)
-    t2 = rot3 (-phi)
-    c2'' = t2 <> c2' <> trans t2
-    w = t2 <> t1
-    coeffs = getCoeffs c2''
-    p = (if sg@>1 < 0 then flipud else id) (ident 3)
-
-reduceConicsEmpirical ell1 ell2 = (w, coeffs) where
-    w1 = whitenEllipse ell1
-    (mx,my,_,_,a) = analyzeConic $ Closed $ map l2p $ ht w1 $ conicPoints $ ell2
-    w2 = chooseRot mx my a
-    w = w2 <> w1
-    coeffs = getCoeffs $ fst $ estimateConicRaw $ map l2p $ ht w $ conicPoints $ ell2
+--------------------------------------------------------------------
 
 -- coefficients in
 -- ax^2 + by^2 + x + ey + f
@@ -263,7 +221,6 @@ getCoeffs m' = (a,b,e,f) where
     f = m @@> (2,2)
     e = m @@> (1,2) * 2
 
---------------------------------------------------------------------
 -- Intersection of two ellipses (after common diagonalization)
 -- (based on the idea of elimination of the x^2coefficient in vgl : Geometry Library)
 intersectionReduced prob = map sol (polySolve (coefs prob)) where
@@ -281,20 +238,40 @@ intersectionReduced prob = map sol (polySolve (coefs prob)) where
               b = b':+0
               e = e':+0
               f = f':+0
+
+intersectionCommonCenter (a,b) = [s1,s2,s3,s4] where
+    p = sqrt $ (b+1)/(b-a)
+    q = sqrt $ (-1-a)/(b-a)
+    s1 = [-p,-q]
+    s2 = [-p, q]
+    s3 = [ p,-q]
+    s4 = [ p, q]
+
+intersectionEllipses c1 c2 = sol where
+    (w, c2') = reduceConics c1 c2
+    a = c2'@@>(0,0)
+    b = c2'@@>(1,1)
+    f = c2'@@>(2,2)
+    d = c2'@@>(0,2)
+    sol = if abs(d/a) > 1E-6
+        then htc (comp $ inv w) $ intersectionReduced (getCoeffs c2')
+        else htc (comp $ inv w) $ intersectionCommonCenter ((a/f):+0,(b/f):+0)
+
+tangentEllipses c1 c2 = map (++[1]) $ intersectionEllipses (inv c1) (inv c2)
+
 --------------------------------------------------------------------
 
 getHorizs pts = map linf pts where
     linf [x,y] = toList $ unitary $ snd $ fromComplex $ cross v (conj v)
         where v = fromList [x,y,1]
 
-shLine [a,b,c] = renderPrimitive Lines $ mapM f [-1,1]
-    where f x = vertex $ Point x ((-a*x-c)/b)
-
 selectSol (x1,y1,_,_,_) (x2,y2,_,_,_) pts = (ij,other) where
     ls = getHorizs pts
-    ij    = head [v | (v,l) <- zip pts ls, f l (x1,y1) * f l (x2,y2) > 0 ]
-    other = head [v | (v,l) <- zip pts ls, f l (x1,y1) * f l (x2,y2) < 0 ]
+    ij    = mbhead [v | (v,l) <- zip pts ls, f l (x1,y1) * f l (x2,y2) > 0 ]
+    other = mbhead [v | (v,l) <- zip pts ls, f l (x1,y1) * f l (x2,y2) < 0 ]
     f [a,b,c] (x,y) = a*x + b*y + c
+    mbhead [] = Nothing
+    mbhead x  = Just (head x)
 
 --------------------------------------------------------------------
 
@@ -320,5 +297,3 @@ orthoTest [x,y] = innerLines n0 h where
 
 innerLines l m = (l.*.m)/ sqrt (l.*.l) / sqrt(m.*.m)
     where a.*.b = a <> mS <.> b
-
-control = Closed [Point (0.5+0.4*cos t) (0.5+0.1*sin t) | t <- tail $ toList (linspace 20 (0,2*pi))]
