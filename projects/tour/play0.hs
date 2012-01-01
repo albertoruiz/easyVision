6200bc34811f6fc3e6b178410787d65fc2cdc947
Alberto Ruiz <aruiz@um.es>
2012-01-01 13:13:06 +0100

    readme and build order

--- projects/tour/play0.hs
+++ projects/tour/play0.hs
@@ -2,7 +2,7 @@ import EasyVision
  
 main = run $ camera >>= f >>= timeMonitor
  
-f :: IO Channels -> IO (IO ImageRGB)
+f :: VC Channels ImageRGB
 f cam = return $ do
     x <- cam
     let r = rgb x
