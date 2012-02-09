0f4ade7e73feb556cfd8037f7715e9a2a381447a
Alberto Ruiz <aruiz@um.es>
2012-02-09 14:45:00 +0100

    hclipping skeleton

---
+++ packages/contrib/clipping/src/Contours/Clipping.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+-----------------------------------------------------------------------------
+{- |
+Module      :  Contours.Clipping
+Copyright   :  (c) PARP Research Group, University of Murcia, 2012
+License     :  GPL
+Maintainer  :  Alberto Ruiz
+Stability   :  provisional
+
+Haskell interface to efficient clipping of arbitrary polygons.
+
+Implementation in C by Adrián Amor Martínez
+
+Algorithm by G. Greiner, K. Hormann, ACM Transactions on Graphics.
+
+-}
+-----------------------------------------------------------------------------
+
+module Contours.Clipping (
+    clip
+)
+where
+
+import ImagProc.Base
+
+clip :: Polyline -> Polyline -> [Polyline]
+-- ^ compute the intersection of two polygons
+clip = undefined
+
