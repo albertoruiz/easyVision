d6598f640fdc80787b1a2972e69b89545e6bc391
Alberto Ruiz <aruiz@um.es>
2012-03-19 19:58:02 +0100

    loading options for clickpoints

--- projects/tour/clickPoints.hs
+++ projects/tour/clickPoints.hs
@@ -1,19 +1,21 @@
 import Vision.GUI
 import ImagProc
-import System.Environment(getArgs)
-import Data.Maybe(listToMaybe)
 import Data.Traversable(traverse)
+import Util.Options(optionFromFile,getRawOption)
 
 main = do
-    mbfile <- listToMaybe `fmap` getArgs
-    mbimg <- traverse loadRGB mbfile
+    mbimg <- getRawOption "--image" >>= traverse loadRGB
+    pts <- optionFromFile "--points" []
+    
     runIt $ do
         p <- clickPoints (sh mbimg)
-        w <- browser "rectified" [] (const id)
+        putW p pts
+        w <- browser "work with them" [] (const id)
         connectWith g p w
 
 sh mbimg pts = Draw [ Draw mbimg
                     , color yellow . drawPointsLabeled $ pts]
 
-g (k,_) ps = (k, [pointSz 5 ps, Draw (Closed ps)])
+g (k,_) ps = (k, [ pointSz 5 ps
+                 , Draw (Closed ps)])
 
