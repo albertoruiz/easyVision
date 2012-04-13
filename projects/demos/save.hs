25eac1252d93a993e829138f00aa276a23112013
Alberto Ruiz <aruiz@um.es>
2012-04-13 19:18:41 +0200

    ok save.hs

--- projects/demos/save.hs
+++ projects/demos/save.hs
@@ -7,7 +7,7 @@ import Vision.GUI
 import ImagProc
 import ImagProc.Camera
 
-main = run saveWin2
+main = run saveWin
 
 main2 = run $   observe "Save" rgb
             >>> arr yuv >>> arrL (take 100) >>> saver "saved.y4m"
@@ -19,7 +19,6 @@ selectSave = clickTag id id sh "Click to save"
     sh (Right x) = Draw [ Draw x, color red $ text (Point 0 0) "RECORDING..."]
     sh (Left  x) = Draw x
 
------------------------------------------------
 
 saveWin = proc img -> do
     y <- selectSave -< yuv img
@@ -35,18 +34,4 @@ saver name = transUI $ do
             x <- cam
             saveYUV4Mpeg f x
             return x
-
-------------------------------------------------
-
-saveWin2 = arr yuv >>> selectSave >>> saver2 "saved.y4m"
-
-saver2 :: FilePath -> ITrans (Either ImageYUV ImageYUV) ImageYUV
-saver2 name = transUI $ do
-    f <- yuvHeader 480 640 name
-    return $ \cam -> do
-            x <- cam
-            case x of
-                Right y -> do saveYUV4Mpeg f y
-                              return y
-                Left y -> return y
 
