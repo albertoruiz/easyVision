f007d45bc8aa7baa56aa8f082af14a2ac7ef20db
Alberto Ruiz <aruiz@um.es>
2013-05-18 14:31:59 +0200

    laSBAg

--- packages/vision/lib/Vision/LASBA.hs
+++ packages/vision/lib/Vision/LASBA.hs
@@ -1,5 +1,5 @@
 module Vision.LASBA(
-    laSBA
+    laSBA, laSBAg
 ) where
 
 import Numeric.LinearAlgebra as LA hiding (i)
@@ -14,18 +14,19 @@ import System.IO.Unsafe(unsafePerformIO)
 
 -- | Interface to sparse bundle adjustment implementation by M.I.A. Lourakis and A.A. Argyros
 laSBA' :: [Proj]
-      -> [Vec] -- ^ homog 3D points
-      -> [Mat] -- ^ calibrated cameras
-      -> Mat   -- ^ common calibration matrix
-      -> ([Vec],[Mat]) -- ^ refined points and cameras
-laSBA' prjs pts cams kal = unsafePerformIO $ do   
+       -> [Vec] -- ^ homog 3D points
+       -> [Mat] -- ^ calibrated cameras
+       -> Mat   -- ^ common calibration matrix
+       -> String -- ^ additional arguments to the executable
+       -> ([Vec],[Mat]) -- ^ refined points and cameras
+laSBA' prjs pts cams kal epilog = unsafePerformIO $ do
    (name,h) <- openTempFile "/tmp" "bundle"   
    let names@[fc,fp,fk,rc,rp] = map (name++) $ words "cams.txt pts.txt kal.txt refcams.txt refpts.txt"
    saveQCams fc cams
    saveRawTracks fp (pts,prjs)
    saveMatrix fk "%f" kal
    putStr "\^[[2m"
-   _ <- system $ "lasba "++unwords [fc,fp,fk,rc,rp] ++" 2> /dev/null" -- ++ " > /dev/null"
+   _ <- system $ "lasba "++unwords [fc,fp,fk,rc,rp] ++ epilog
    putStr "\^[[0m"
    refcams <- loadQCams rc
    refpts <- loadMatrix rp
@@ -34,13 +35,23 @@ laSBA' prjs pts cams kal = unsafePerformIO $ do
    return (toRows (refpts&1),refcams)
 
 -- | Interface to sparse bundle adjustment implementation by M.I.A. Lourakis and A.A. Argyros
+laSBAg :: String -- ^ additional arguments to the executable
+       -> [Proj]
+       -> [Vec] -- ^ homog 3D points
+       -> [Mat] -- ^ calibrated cameras
+       -> [Mat] -- ^ calibration matrices
+       -> ([Vec],[Mat]) -- ^ refined points and cameras
+laSBAg epilog prjs pts cams [k]  = laSBA' prjs pts cams k epilog
+laSBAg epilog prjs pts cams kals = laSBA' kprjs pts cams k epilog
+  where
+    kprjs = calibrateProjections (map (<> inv k) kals) prjs
+    k = diag (vec [1000,1000,1])
+
+-- | laSBA \" 2> /dev/null\"
 laSBA :: [Proj]
       -> [Vec] -- ^ homog 3D points
       -> [Mat] -- ^ calibrated cameras
       -> [Mat] -- ^ calibration matrices
       -> ([Vec],[Mat]) -- ^ refined points and cameras
-laSBA prjs pts cams [k]  = laSBA' prjs pts cams k
-laSBA prjs pts cams kals = laSBA' kprjs pts cams k where
-    kprjs = calibrateProjections (map (<> inv k) kals) prjs
-    k = diag (vec [1000,1000,1])
+laSBA  = laSBAg " "
 
