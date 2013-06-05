57723191100df97fab3d32b6b9ce732ca3fe7d45
Alberto Ruiz <aruiz@um.es>
2013-06-05 18:56:58 +0200

    show multiple resection methods

--- projects/vision/geom/resection4.hs
+++ projects/vision/geom/resection4.hs
@@ -8,6 +8,7 @@ import Util.Geometry
 import Util.Camera
 import Util.Options
 import Util.Misc(posMin, replaceAt)
+import Vision.Camera(refineNewton)
 
 main = do
     mbimg <- getRawOption "--image" >>= traverse loadRGB
@@ -24,24 +25,33 @@ sh ref mbimg pts = Draw [ Draw $ fmap (rgb.channelsFromRGB) mbimg
   where
     cam = computeCamera pts ref
 
-g ref mbimg (n,_) (ps,_) = (n, [ clearColor white
-                                 [ color gray $ axes3D 4
-                                 , color red . pointSz 3 $ drawPoints3DLabeled ref
-                                 , drcam
-                                 , pointSz 5 . color orange $ ipts
-                                 , color lightgray rays
-                               ]
-                            ])
+g ref mbimg (n,_) (ps,_) = (n, [ Draw $ thing computeCamera
+                               , Draw $ thing (computeLinearPose 1.7)
+                               , Draw $ thing (optimal 1.7)
+                               , Draw $ thing (computeLinearPose 2.5)
+                               ])
   where
-    drcam | length ps < length ref = Draw ()
-          | otherwise = color green $ showCam 2 ic (fmap (float.grayscale.channelsFromRGB) mbimg)
-    -- cam = computeCamera ps ref
-    cam = computeLinearPose 1.7 ps ref
-    ic = infoCam cam 
-    ipts = toImagePlane ic 2 ps
-    invc = invTrans cam
-    rays = invc <| map homog ps
+    thing method = [ clearColor white
+                        [ color gray $ axes3D 4
+                        , color red . pointSz 3 $ drawPoints3DLabeled ref
+                        , drcam
+                        , pointSz 5 . color orange $ ipts
+                        , color lightgray rays
+                        ]
+            ]
+      where
+        drcam | length ps < length ref = Draw ()
+              | otherwise = color green $ showCam 2 ic (fmap (float.grayscale.channelsFromRGB) mbimg)
 
+        cam = method ps ref
+        ic = infoCam cam
+        ipts = toImagePlane ic 2 ps
+        invc = invTrans cam
+        rays = invc <| map homog ps
+
+withRawMat m ps ref = unsafeFromMatrix $ m (homog $ datMat ps) (homog $ datMat ref)
+
+optimal f ps ref = unsafeFromMatrix $ refineNewton 5 (toMatrix $ computeLinearPose f ps ref) (homog $ datMat ps) (homog $ datMat ref)
 
 --------------------------------------------------------------------------------
 
