ae7f5076fd87d37f3b0cbcd74a2550e5a6416bda
Alberto Ruiz <aruiz@um.es>
2015-02-01 12:59:48 +0100

    opencv warp

--- packages/contrib/opencv/src/OpenCV.hs
+++ packages/contrib/opencv/src/OpenCV.hs
@@ -6,14 +6,18 @@ module OpenCV(
   cascadeClassifier,
   solvePNP,
   findHomography,
-  surf
+  surf,
+  warp8u, warp8u3, warp32f
 ) where
 
 import Image.Devel
 import Foreign.Ptr
 import Foreign.Marshal
 import Foreign.C.String
+import Foreign(Word8)
 import Foreign.Storable
+import Foreign.ForeignPtr(withForeignPtr)
+import Foreign.ForeignPtr.Unsafe(unsafeForeignPtrToPtr)
 import Util.Geometry(Segment(..),Point(..))
 import Control.Applicative
 import Data.Packed.Development
@@ -48,6 +52,52 @@ canny = wrap11S "opencv_canny" c_opencv_canny
 foreign import ccall "opencv_canny"
     c_opencv_canny :: RawImageS I8u (RawImageS I8u (IO CInt))
 
+------------------------------------------------------------------
+
+warp8u :: Word8 -> Size -> Matrix Double -> Image I8u -> Image I8u
+warp8u g = wrap11SHS "opencv_warp8u" (c_opencv_warp8u g)
+
+warp8u3 :: Word8 -> Size -> Matrix Double -> Image I8u3 -> Image I8u3
+warp8u3 g = wrap11SHS "opencv_warp8u3" (c_opencv_warp8u3 g)
+
+warp32f :: Float -> Size -> Matrix Double -> Image Float -> Image Float
+warp32f g = wrap11SHS "opencv_warp32f" (c_opencv_warp32f g)
+
+
+type MAT t = CInt -> CInt -> Ptr Double -> t
+
+withCMatrix :: Matrix Double -> IO b -> IO b
+withCMatrix m act 
+   | orderOf m == RowMajor = withForeignPtr fp $ \_ -> act
+   | otherwise = error "withMatrix ColumnMajor"
+  where
+    (fp,_,_) = unsafeToForeignPtr (flatten m)
+
+appMat :: MAT t -> Matrix Double -> t
+appMat f m = f r c p 
+  where
+    r = fi (rows m)
+    c = fi (cols m)
+    (fp,_,_) = unsafeToForeignPtr (flatten m)
+    p = unsafeForeignPtrToPtr fp
+
+wrap11SHS :: Storable b => String -> MAT (Wrap11S a b) -> Size -> Matrix Double -> Image a -> Image b
+wrap11SHS msg f sz m x = unsafePerformIO $ do
+    r <- newImage undefined sz
+    let cm = cmat $ inv (pixelToPointTrans (size r)) <> m <> pixelToPointTrans (size x)
+    withCMatrix cm $ withImage x $ withImage r $ checkFFI msg $
+        f `appMat` cm `appS` x `appS` r
+    return r
+
+foreign import ccall "opencv_warp8u"
+    c_opencv_warp8u :: Word8 -> MAT (RawImageS I8u (RawImageS I8u (IO CInt)))
+
+foreign import ccall "opencv_warp8u3"
+    c_opencv_warp8u3 :: Word8 -> MAT (RawImageS I8u3 (RawImageS I8u3 (IO CInt)))
+
+foreign import ccall "opencv_warp32f"
+    c_opencv_warp32f :: Float -> MAT (RawImageS Float (RawImageS Float (IO CInt)))
+    
 ------------------------------------------------------------------
 
 foreign import ccall "hough"
--- packages/contrib/opencv/src/OpenCV/opencv1.c
+++ packages/contrib/opencv/src/OpenCV/opencv1.c
@@ -16,12 +16,102 @@ void opencv_canny(IMGSZ(s),IMGSZ(d)) {
     cvReleaseImageHeader(&ipl_d);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+void opencv_warp8u(int g, int r, int c, double*p, IMGSZ(s),IMGSZ(d)) {
+#define AT(i,j) (p[(i)*c+(j)]) 
+
+    IPL(s,1)
+    IPL(d,1)
+
+    CvMat* h = cvCreateMat(3, 3, CV_32F);
+    cvSetReal2D(h, 0, 0, AT(0,0));
+    cvSetReal2D(h, 0, 1, AT(0,1));
+    cvSetReal2D(h, 0, 2, AT(0,2));
+    cvSetReal2D(h, 1, 0, AT(1,0));
+    cvSetReal2D(h, 1, 1, AT(1,1));
+    cvSetReal2D(h, 1, 2, AT(1,2));
+    cvSetReal2D(h, 2, 0, AT(2,0));
+    cvSetReal2D(h, 2, 1, AT(2,1));
+    cvSetReal2D(h, 2, 2, AT(2,2));
+
+    cvWarpPerspective(ipl_s, ipl_d, h, CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS, cvScalarAll(g) );
+
+    cvReleaseImageHeader(&ipl_s);
+    cvReleaseImageHeader(&ipl_d);
+
+#undef AT
+}
+
+//---------------------------------------------------
+
+// FIXME in wrappers.h and update here
+#undef IPL
+#define IPL(X,S,C) IplImage * ipl_##X = cvCreateImageHeader(cvSize(X##width,X##height), S, C ); \
+                 ipl_##X->imageData = X##p; \
+                 ipl_##X->widthStep = X##step; \
+                 cvSetImageROI(ipl_##X,cvRect(X##c1,X##r1,X##c2-X##c1+1,X##r2-X##r1+1));
+
+void opencv_warp32f(float g, int r, int c, double*p, IMGSZ(s),IMGSZ(d)) {
+#define AT(i,j) (p[(i)*c+(j)]) 
+
+    IPL(s,32,1)
+    IPL(d,32,1)
+
+    CvMat* h = cvCreateMat(3, 3, CV_32F);
+    cvSetReal2D(h, 0, 0, AT(0,0));
+    cvSetReal2D(h, 0, 1, AT(0,1));
+    cvSetReal2D(h, 0, 2, AT(0,2));
+    cvSetReal2D(h, 1, 0, AT(1,0));
+    cvSetReal2D(h, 1, 1, AT(1,1));
+    cvSetReal2D(h, 1, 2, AT(1,2));
+    cvSetReal2D(h, 2, 0, AT(2,0));
+    cvSetReal2D(h, 2, 1, AT(2,1));
+    cvSetReal2D(h, 2, 2, AT(2,2));
+
+    cvWarpPerspective(ipl_s, ipl_d, h, CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS, cvScalarAll(g) );
+
+    cvReleaseImageHeader(&ipl_s);
+    cvReleaseImageHeader(&ipl_d);
+
+#undef AT
+}
+
+//-----------------------------------------------
+
+void opencv_warp8u3(float g, int r, int c, double*p, IMGSZ(s),IMGSZ(d)) {
+#define AT(i,j) (p[(i)*c+(j)]) 
+
+    IPL(s,8,3)
+    IPL(d,8,3)
+
+    CvMat* h = cvCreateMat(3, 3, CV_32F);
+    cvSetReal2D(h, 0, 0, AT(0,0));
+    cvSetReal2D(h, 0, 1, AT(0,1));
+    cvSetReal2D(h, 0, 2, AT(0,2));
+    cvSetReal2D(h, 1, 0, AT(1,0));
+    cvSetReal2D(h, 1, 1, AT(1,1));
+    cvSetReal2D(h, 1, 2, AT(1,2));
+    cvSetReal2D(h, 2, 0, AT(2,0));
+    cvSetReal2D(h, 2, 1, AT(2,1));
+    cvSetReal2D(h, 2, 2, AT(2,2));
+
+    cvWarpPerspective(ipl_s, ipl_d, h, CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS, cvScalarAll(g) );
+
+    cvReleaseImageHeader(&ipl_s);
+    cvReleaseImageHeader(&ipl_d);
+
+#undef AT
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void hough(IMGSZ(t),int fmax, int* fn, TSegment* res) {
 
-    IPL(t,1)
+    IPL(t,8,1)
 
     CvMemStorage* storage = cvCreateMemStorage(0);
     CvSeq* lines = 0;
